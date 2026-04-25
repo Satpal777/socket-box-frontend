d7050653de790baee9fa31e4988eb52f08e1d4c2
Satpal777 <rsshacker777@gmail.com>
2026-04-25 14:28:01 +0530

    fix : Config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        }
       }
     }
-  }
-})
+  })
+}
